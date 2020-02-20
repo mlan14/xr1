755ef52b6a6a7045e9e97085f766e6b685c0bbd7
mlan14 <57930585+mlan14@users.noreply.github.com>
2020-02-19 19:48:45 -0800

    Initial commit

---
+++ m03/code_week3/curve2/js/audioanalyser-beat-scale.js
@@ -0,0 +1,23 @@
+FRAME.registerComponent('audioanalyser-beat-scale', {
+  schema: {
+    analyserEl: {type: 'selector'}
+  },
+
+  init: function () {
+    var analyserEl = this.data.analyserEl || this.el;
+    var el = this.el;
+
+    analyserEl.addEventListener('audioanalyser-beat', function () {
+      el.setAttribute('scale', {
+        x: volume,
+        y: volume,
+        z: volume
+      });
+      el.setAttribute('rotation', {
+        x: volume,
+        y: volume,
+        z: volume
+      });
+    });
+  }
+});
